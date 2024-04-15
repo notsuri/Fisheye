c6add6cad1a7ba112abf0003ebc4eaa5dca03963
Notsuri <notsugit@gmail.com>
2024-04-15 18:31:28 +0200

    Ajout des élément manquent sur la card et photo des photographe

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,36 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/portrait/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const p = document.createElement("p");
+    p.textContent = city + "," + country;
+
+    const taglineElement = document.createElement("p");
+    taglineElement.textContent = tagline;
+
+    const priceElement = document.createElement("p");
+    priceElement.textContent = price + "€ /jour";
+
+    const url = document.createElement("a");
+    // url.href = `./photographer.html?id=${id}`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(p);
+    article.appendChild(taglineElement);
+    article.appendChild(priceElement);
+    // article.appendChild(url);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
